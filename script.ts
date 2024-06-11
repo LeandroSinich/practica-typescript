c11e245320de3796120f384bfc2275857eb41eeb
LeandroSinich <leandrosinich@gmail.com>
2024-06-10 21:30:03 -0300

    agregando navegacion

--- script.ts
+++ script.ts
@@ -1,1 +1,5 @@
-console.log('probando modo observador');
+import {home,variables} from './informacion'
+
+let subtitulo = document.getElementById('subtitulo');
+let informacion = document.getElementById('info');
+
